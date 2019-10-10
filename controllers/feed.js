084fc2c637867903092753e4a743b2479190849f
allayGerald <geraldallay@gmail.com>
2019-10-10 18:07:23 +0300

    Fetch single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -47,3 +47,27 @@ exports.createPost = (req, res, next) => {
     });
 
 }
+
+
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+
+      res.status(200).json({post: post});
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+
+      next(err);
+    });
+}
--- routes/feed.js
+++ routes/feed.js
@@ -5,9 +5,12 @@ const router = express.Router();
 const feedController = require('../controllers/feed');
 
 router.get('/posts', feedController.getPosts);
+
 router.post('/post', [
-    body('content').trim().isLength({min: 5}),
-    body('title').trim().isLength({min: 5})
+    body('content').trim().isLength({ min: 5 }),
+    body('title').trim().isLength({ min: 5 })
 ], feedController.createPost);
+
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;
