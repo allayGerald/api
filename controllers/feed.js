c17680972a7801c53e1bbfef70f8e28ff066b904
allayGerald <geraldallay@gmail.com>
2019-10-10 16:54:26 +0300

    create Post to db

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,8 @@
 
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/posts');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{ title: 'First Post', content: 'This is the first post!' }]
@@ -8,8 +10,6 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -19,8 +19,25 @@ exports.createPost = (req, res, next) => {
     });
   }
 
-  res.status(200).json({
-    message: 'Post created successfully! ',
-    posts: [{ title: title, content: content }]
+  const title = req.body.title;
+  const content = req.body.content;
+  const imageUrl = req.body.imageUrl;
+
+  const post = new Post({
+    title: title,
+     content: content,
+     imageUrl: imageUrl,
+     creator: {name: 'Gerald'}
   })
+
+  post.save()
+  .then(newPost => {
+    console.log(newPost);
+    res.status(200).json({
+      message: 'Post created successfully! ',
+      posts: newPost
+    });
+  })
+.catch(err => console.log(err));
+ 
 }
