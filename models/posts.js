ebbda977b22728f31cc3838706a45d6440086ad0
allayGerald <geraldallay@gmail.com>
2019-10-10 17:27:53 +0300

    custom validation

---
+++ models/posts.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String
+    },
+    creator: {
+        type: Object,
+        required: true
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
